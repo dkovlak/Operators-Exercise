7501473efe785d4411d6ff84145bd1de8cf1465d
David Kovlak <kovlakdavid4@gmail.com>
2023-08-28 12:50:12 -0700

    Radius

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+
+            Console.WriteLine("What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of a cirle with radius of {radius} is {AreaOfCircle(radius):F2}");
+        }
+
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
+
